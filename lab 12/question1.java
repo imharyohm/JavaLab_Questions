5f09b19cfed55cf5e312af9dd490715645fc71b4
imharyohm <imharyohm@gmail.com>
2023-04-23 11:56:35 +0530

    updated

--- lab 12/question1.java
+++ lab 12/question1.java
@@ -5,18 +5,9 @@
 import java.io.*;
 public class question1{
     public static void main(String[] args) throws IOException{
-        // using FileOutputStream  with byte array
-        String str = "Name: Hariom\nRoll No: 21053282\nBranch: CSE\n";
-        FileOutputStream fout = new FileOutputStream("bio.txt");
-        fout.write(str.getBytes());
-        fout.close();
 
-        // using FileInputStream
-        FileInputStream fin = new FileInputStream("bio.txt");
-        int i;
-        while((i = fin.read()) != -1){
-            System.out.print((char)i);
-        }
+        
+    }
         fin.close();
     
         //using FileWriter with String
